fdca6d3e479c50f4100d4561c5f8ab5f1b541a5d
jasur737 <baxtiyorovjasur728@gmail.com>
2025-07-15 10:32:41 +0500

    ;)

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-fetch("https://jsonplaceholder.typicode.com/comments/1")
+fetch("https://jsonplaceholder.typicode.com/users/1")
   .then((response) => response.json())
-  .then((data) => console.log(data));
-  
-  
+  .then((data) => {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    document.getElementById("user-name").textContent = `Username: ${data.username}`
+    document.getElementById("phone-number").textContent = `Phone: ${data.phone}`
+    document.getElementById("add").textContent = `Address: ${data.address.city}`
+    name.textContent = `Name: ${data.name}`;
+    email.textContent = `Email: ${data.email}`;
+  });
+//   .catch((error) => (console.error("Xatolik ", error))
+//  )
