9a6f7c226d7bdfb67fd10299bb9519ba4d40286f
jasur737 <baxtiyorovjasur728@gmail.com>
2025-07-19 10:32:17 +0500

    ;)

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((response) => response.json())
-  .then((users) => {
-    const container = document.getElementById("container")
-    users.forEach((users) => {
-      const card = document.createElement("div");
-      card.className = "card"
-      card.innerHTML = `
-      <h2>Name: ${users.name}</h2>
-      <h3>Username: ${users.username}</h3>
-      <p>Email: ${users.email}</p>
-      <P>Street: ${users.address.street}</P>
-      <P>Phone: ${users.phone}</P>
-      <a href="">Website: ${users.Website}</a>
-      <P>Company: ${users.company.name}</P>
-      `;
-      container.appendChild(card);
-    });
-  });
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((users) => {
+//     const container = document.getElementById("container")
+//     users.forEach((users) => {
+//       const card = document.createElement("div");
+//       card.className = "card"
+//       card.innerHTML = `
+//       <h2>Name: ${users.name}</h2>
+//       <h3>Username: ${users.username}</h3>
+//       <p>Email: ${users.email}</p>
+//       <P>Street: ${users.address.street}</P>
+//       <P>Phone: ${users.phone}</P>
+//       <a href="">Website: ${users.Website}</a>
+//       <P>Company: ${users.company.name}</P>
+//       `;
+//       container.appendChild(card);
+//     });
+//   });
 //   .catch((error) => (console.error("Xatolik ", error))
 //  )
+
+fetch('https://randomfox.ca/floof/')
+    .then(response => response.json())
+    .then(data => {
+      const container = document.getElementById("container")
+      container.innerHTML = `
+      <img src="${data.image}"/>
+      `
+    });
+
+
+
